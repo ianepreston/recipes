c485178ab9a9530153f6928d09da2c8469d0b942
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-05-18 14:55:53 -0600

    error handling and colours

--- arch_bootstrap/bootstrap.sh
+++ arch_bootstrap/bootstrap.sh
@@ -2,4 +2,19 @@
 
 # To install: bash <(curl -fsSL http://bootstrap.ianpreston.ca)
 
-echo "We got this far!"
+SOURCED=false && [ "${0}" = "${BASH_SOURCE[0]}" ] || SOURCED=true
+if ! $SOURCED; then
+  set -eEu
+  shopt -s extdebug
+  trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+  IFS=$'\n\t'
+fi
+
+# Text modifiers
+Bold="\033[1m"
+Reset="\033[0m"
+
+# Colors
+Red="\033[31m"
+Green="\033[32m"
+Yellow="\033[33m"
