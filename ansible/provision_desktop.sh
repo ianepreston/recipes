422eec0d75a6085706eba011935a5fcc9119970f
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-11-16 15:35:48 -0700

    switch between partial and full

--- ansible/provision_desktop.sh
+++ ansible/provision_desktop.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Provision localhost with Ansible
-ansible-playbook test.yml #--ask-become-pass
+# ansible-playbook test.yml #--ask-become-pass
+ansible-playbook main.yml #--ask-become-pass
