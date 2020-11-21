492d89d508fa4dfe7474dd8bbf7fd9721dc51bb3
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2020-11-21 16:49:48 -0700

    set up for full run

--- ansible/provision_desktop.sh
+++ ansible/provision_desktop.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-# Provision localhost with Ansible
-ansible-playbook test.yml #--ask-become-pass
-# ansible-playbook main.yml #--ask-become-pass
+# ansible-playbook main.yml --ask-become-pass
